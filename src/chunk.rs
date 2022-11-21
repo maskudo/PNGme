aa832ffbe0d17481aa64a66c94d3b7f4111bfd4d
maskudo <manujkarki101@gmail.com>
2022-11-21 23:13:30 +0545

    implement functions

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,6 +1,9 @@
+use core::fmt;
+use crc::CRC_32_ISO_HDLC;
 use std::fmt::Display;
 
-use crate::chunk_type::{ChunkType};
+use crate::chunk_type::ChunkType;
+use crate::{Error, Result};
 
 #[derive(Debug, Clone)]
 pub struct Chunk {
@@ -10,39 +13,96 @@ pub struct Chunk {
     crc: u32,
 }
 
-impl TryFrom<&[u8]> for Chunk {}
+fn slice_to_array(slice: &[u8]) -> [u8;4] {
+    let mut arr = [0 as u8;4];
+    for (i, item) in slice.iter().enumerate(){
+        arr[i] = *item;
+    }
+    arr
+}
+impl TryFrom<&[u8]> for Chunk {
+    type Error = Error;
+
+    fn try_from(bytes: &[u8]) -> Result<Self> {
+        let chunk_type = ChunkType::try_from(slice_to_array(&bytes[4..8]))?;
+        let data : Vec<u8>= (&bytes[8..(bytes.len()-4)]).to_vec();
+        let crc_from_slice = u32::from_be_bytes(slice_to_array(&bytes[bytes.len()-4..]));
+
+        let chunk = Chunk::new(chunk_type, data);
+        if chunk.crc() != crc_from_slice{
+            return Err(From::from("Wrong CRC"));
+        }
+
+        Ok(chunk)
+    }
+}
 
 impl Display for Chunk {
-    
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Chunk{{",)?;
+        writeln!(f, " Length: {}", self.length())?;
+        writeln!(f, " Type: {}", self.chunk_type())?;
+        writeln!(f, " Data: {}", self.data().len())?;
+        writeln!(f, " Crc: {}", self.crc())?;
+        writeln!(f, "}}",)?;
+        Ok(())
+    }
 }
 
 impl Chunk {
     fn new(chunk_type: ChunkType, data: Vec<u8>) -> Chunk {
-        unimplemented!();
+        let chunk_and_data: Vec<u8> = chunk_type
+            .bytes()
+            .iter()
+            .chain(data.iter())
+            .copied()
+            .collect();
+        let crc = crc::Crc::<u32>::new(&CRC_32_ISO_HDLC);
+        let crc = crc.checksum(&chunk_and_data[..]);
+        Chunk {
+            length: data.len() as u32,
+            chunk_type,
+            data,
+            crc,
+        }
     }
 
     fn length(&self) -> u32 {
-        unimplemented!();
+        self.length
     }
 
     fn chunk_type(&self) -> &ChunkType {
-        unimplemented!();
+        &self.chunk_type
     }
 
     fn data(&self) -> &[u8] {
-        unimplemented!();
+        &self.data
     }
 
     fn crc(&self) -> u32 {
-        unimplemented!();
+        self.crc
     }
 
-    fn data_as_string(&self) -> Result<String, String> {
-        unimplemented!();
+    fn data_as_string(&self) -> Result<String> {
+        match String::from_utf8(self.data.clone()){
+            Ok(s) => Ok(s),
+            Err(e) => Err(From::from(e))
+        }
     }
 
     fn as_bytes(&self) -> Vec<u8> {
-        unimplemented!();
+        let len_bytes = self.length.to_be_bytes();
+        let ch_bytes = self.chunk_type.bytes();
+        let crc_bytes = self.crc.to_be_bytes();
+        let total: Vec<u8> = len_bytes
+            .iter()
+            .chain(ch_bytes.iter())
+            .chain(self.data.iter())
+            .chain(crc_bytes.iter())
+            .copied()
+            .collect();
+        
+        total
     }
 }
 
