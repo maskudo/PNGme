8a8a5c942573da29839d92af65ffe347343fac4a
maskudo <manujkarki101@gmail.com>
2022-11-25 19:49:20 +0545

    add commands

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ mod png;
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
-fn main() {
-    println!("Hello, PNGme!");
+use args::Cli;
+use args::Commands;
+use clap::Parser;
+
+fn main() -> Result<()>{
+    let cli: Cli = Cli::parse();
+    match &cli.command{
+        Commands::Encode(args) => commands::encode(args)?,
+        Commands::Decode(args) => commands::decode(args)?,
+        Commands::Print(args) => commands::print(args)?,
+        Commands::Remove(args) => commands::remove(args)?,
+    };
+    Ok(())
 }
